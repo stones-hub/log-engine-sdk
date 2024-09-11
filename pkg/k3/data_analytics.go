0cf49e8710c79dc26a80a8ccdc5dea6cf5a23567
STones_ <61647649@qq.com>
2024-09-11 18:01:22 +0800

    add

--- pkg/k3/data_analytics.go
+++ pkg/k3/data_analytics.go
@@ -43,7 +43,15 @@ func (i *DataAnalytics) CleanSuperProperties() {
 	i.superProperties = make(map[string]interface{})
 }
 
-func (i *DataAnalytics) track() {
+func (i *DataAnalytics) Track(accountId, appId, eventName, eventId, ip string, properties map[string]interface{}) error {
+	return i.track(accountId, appId, eventName, eventId, ip, properties)
+}
+
+func (i *DataAnalytics) track(accountId, appId, eventName, eventId, ip string, properties map[string]interface{}) error {
+	var (
+		msg string
+		p   map[string]interface{}
+	)
 
 	defer func() {
 		if r := recover(); r != nil {
@@ -51,22 +59,29 @@ func (i *DataAnalytics) track() {
 		}
 	}()
 
-	// TODO 日志封装 思考
-}
-
-func (i *DataAnalytics) add(accountId, appId, eventName, eventId, ip string, properties map[string]interface{}) error {
-	var (
-		msg  string
-		now  string
-		uuid string
-		data protocol.Data
-	)
-
 	if len(accountId) == 0 {
 		msg = "invalid parameters: account_id cannot be empty "
 		K3LogError(msg)
 		return errors.New(msg)
 	}
+
+	if len(eventId) == 0 {
+		msg = "the event id must be provided"
+		K3LogError(msg)
+		return errors.New(msg)
+	}
+
+	p = i.GetSuperProperties()
+	MergeProperties(p, properties)
+	return i.add(accountId, appId, eventName, eventId, ip, p)
+}
+
+func (i *DataAnalytics) add(accountId, appId, eventName, eventId, ip string, properties map[string]interface{}) error {
+	var (
+		now  string
+		uuid string
+		data protocol.Data
+	)
 
 	now = time.Now().Format("2006-01-02 15:04:05")
 	uuid = GenerateUUID()
