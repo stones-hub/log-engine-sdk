f4bc10e6689c968b8e221882a15eeafb0fe23f22
STones_ <61647649@qq.com>
2024-09-27 17:05:05 +0800

    add

--- pkg/k3/protocol/data.go
+++ pkg/k3/protocol/data.go
@@ -2,12 +2,24 @@ package protocol
 
 // ElkLogData 存入到elk的数据日志
 type ElkLogData struct {
+	UUID      string    // uuid
 	Host      string    // 来源主机
 	Ip        string    // 来源ip
-	LogType   string    // 日志类型
+	LogLevel  LogLevel  // 日志级别
+	LogType   LogType   // 日志类型
 	EventId   EventID   // 事件id
 	EventName EventName // 事件名称
+	Data      interface{}
 }
+
+type LogLevel string
+
+const (
+	Error LogLevel = "error"
+	Warn  LogLevel = "warn"
+	Info  LogLevel = "info"
+	Debug LogLevel = "debug"
+)
 
 type LogType string
 
@@ -15,6 +27,7 @@ const (
 	Nginx LogType = "nginx"
 	Mysql LogType = "mysql"
 	Redis LogType = "redis"
+	Trace LogType = "trace"
 )
 
 type EventName string
