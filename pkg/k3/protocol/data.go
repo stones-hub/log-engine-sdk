478171199365c9526710066b150648c9e8cf92b8
STones_ <61647649@qq.com>
2024-09-29 14:35:12 +0800

    add

--- pkg/k3/protocol/data.go
+++ pkg/k3/protocol/data.go
@@ -6,15 +6,18 @@ package protocol
 
 // ElkLogData 存入到elk的数据日志
 type ElkLogData struct {
-	UUID      string    // uuid
-	Timestamp string    // 日志生成时间 "2024-10-01 12:01:00"
-	HostName  string    // 来源主机
-	IP        string    // 来源ip
-	LogLevel  LogLevel  // 日志级别
-	LogType   LogType   // 日志类型
-	EventId   EventID   // 事件id
-	EventName EventName // 事件名称
-	Data      interface{}
+	UUID      string      // uuid
+	Timestamp string      // 日志生成时间 "2024-10-01 12:01:00"
+	HostName  string      // 来源主机
+	IP        string      // 来源ip
+	HttpCode  int         // http状态码
+	UseAgent  string      // 浏览器信息
+	LogLevel  LogLevel    // 日志级别
+	LogType   LogType     // 日志类型
+	EventId   EventID     // 事件id
+	EventName EventName   // 事件名称
+	TraceId   string      // traceId， 用于解决连续性日志, 串连问题
+	Data      interface{} // 无需拆分的日志
 }
 
 type LogLevel string
