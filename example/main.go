163eb0459332527bef7dc469d690834c276de07a
STones <61647649@qq.com>
2024-10-10 23:17:37 +0800

    add

--- example/main.go
+++ example/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+)
 
+var jsonString = `{
+  "name": "John Doe",
+  "age": 30,
+  "city": "New York",
+	"address" : {
+		"org":"china",
+		"provide" :"guangzhou"
+	}
+}`
+
+func main() {
+	var (
+		dataMap = make(map[string]interface{})
+		err     error
+	)
+	if err = json.Unmarshal([]byte(jsonString), &dataMap); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(dataMap)
+	}
 }
