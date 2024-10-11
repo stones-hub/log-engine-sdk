477410ba313034a8f96a64de8a136765eae54ae0
STones_ <61647649@qq.com>
2024-10-11 11:27:54 +0800

    add

--- example/main.go
+++ example/main.go
@@ -3,26 +3,68 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log-engine-sdk/pkg/k3/protocol"
 )
 
 var jsonString = `{
-  "name": "John Doe",
-  "age": 30,
-  "city": "New York",
-	"address" : {
-		"org":"china",
-		"provide" :"guangzhou"
-	}
+    "log_level": "INFO", 
+    "host_name" : "ali-gnfx-api-sdk4-01",
+    "_id": "8816c977-854e-11ef-917e-00163e346885",
+    "trace_id": "f0713b0d0ea4b3b5a068a808e2f38aa",
+    "domain" : "gnuser.3k.com",
+    "http_code" : 200,
+    "host_ip" : "192.168.3.130",
+    "code_name": "api_sdk4",
+    "client_ip": "34.12.10.1",
+    "timestamp" : "2024-10-09T17:41:30.703011223+08:00",
+    "org": "gnfx",
+    "log_src": "default",
+    "project" : "ywzx",
+    "event_id" : 1001,
+    "event_name" : "user_login",
+    "extend_data": {
+        "uid": "2029753648",
+        "game_name" : "坦克前线",
+        "amount" : 30,
+        "currency" : "RMB",
+        "language" : "Android",
+        "version" : "10.7.2",
+        "code" : 1001,
+        "content" : {
+            "time_local": "2024-10-09 15:40:03",
+            "channel": "trace",
+            "content": "[trace.server]接收请求POST http://gnuser.3k.com/v5/user/login",
+            "context": {
+                "log_type": "trace.server",
+                "url": "http://gnuser.3k.com/v5/user/login",
+                "method": "POST",
+                "input": "p=nh2%252FcdpyD",
+                "output": {"msg": "Success"},
+                "cost_ms": 49,
+                "start_time": "2024-10-09 15:39:23.644",
+                "end_time": "2024-10-09 15:39:23.693"
+            }
+        }
+    }
 }`
 
 func main() {
 	var (
-		dataMap = make(map[string]interface{})
-		err     error
+		log protocol.ElasticSearchData
+		err error
+		b   []byte
 	)
-	if err = json.Unmarshal([]byte(jsonString), &dataMap); err != nil {
-		fmt.Println(err)
+	if err = json.Unmarshal([]byte(jsonString), &log); err != nil {
+		fmt.Println(err.Error())
+		return
 	} else {
-		fmt.Println(dataMap)
+		fmt.Println(log)
+		fmt.Println("---->")
+		if b, err = json.Marshal(log); err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		fmt.Println(string(b))
+
 	}
 }
--- pkg/k3/protocol/elk_data.go
+++ pkg/k3/protocol/elk_data.go
@@ -1,31 +1,32 @@
 package protocol
 
-type LogLevel string
-
-const (
-	Error LogLevel = "error"
-	Warn  LogLevel = "warn"
-	Info  LogLevel = "info"
-	Debug LogLevel = "debug"
-)
-
 type ElasticSearchData struct {
-	UUID       string     // 日志唯一ID， elk document id
-	Timestamp  string     // 日志产生时间 "2024-10-01 12:00:00 "
-	Protocol   string     // 协议"HTTP/1.1"
-	Domain     string     // 域名
-	LogIp      string     // 日志来源IP
-	HostIp     string     // 日志落盘IP
-	HostName   string     // 日志落盘主机名
-	LogLevel   LogLevel   // 日志级别
-	Org        string     // 事业部
-	Project    string     // 业务线 业务中心， 公司系统, 投放等
-	ExtendData ExtendData // 扩展字段
+	LogLevel   string     `json:"log_level,omitempty"`  // 日志级别
+	HostName   string     `json:"host_name,omitempty"`  // 日志落盘主机名
+	UUID       string     `json:"_id,omitempty"`        // 日志唯一ID， elk document id
+	TraceId    string     `json:"trace_id,omitempty"`   // 追踪ID
+	Domain     string     `json:"domain,omitempty"`     // 域名
+	HttpCode   int        `json:"http_code,omitempty"`  // http状态码
+	HostIp     string     `json:"host_ip,omitempty"`    // 日志落盘IP
+	CodeName   string     `json:"code_name,omitempty"`  // 代码仓库标识
+	ClientIp   string     `json:"client_ip,omitempty"`  // 日志来源IP
+	Timestamp  string     `json:"timestamp,omitempty"`  // 日志产生时间 "2024-10-01 12:00:00 "
+	Org        string     `json:"org,omitempty"`        // 事业部
+	LogSrc     string     `json:"log_src,omitempty"`    // 日志源(内部调用/调用第三方等)
+	Project    string     `json:"project,omitempty"`    // 业务线 业务中心， 公司系统, 投放等
+	EventId    int        `json:"event_id,omitempty"`   // 日志事件ID
+	EventName  string     `json:"event_name,omitempty"` // 日志事件名称(每种日志唯一)
+	Protocol   string     `json:"protocol,omitempty"`   // 协议"HTTP/1.1"
+	ExtendData ExtendData `json:"extend_data"`          // 扩展字段
 }
 
 type ExtendData struct {
-	Uid      string
-	GameName string
-	Amount   int64
-	Content  map[string]interface{} // 自定义给日志打印方，随意key-value的方式即可
+	Uid      string                 `json:"uid,omitempty"`       // 用户ID
+	GameName string                 `json:"game_name,omitempty"` // 游戏名
+	Amount   int64                  `json:"amount,omitempty"`    // 充值金额
+	Currency string                 `json:"currency,omitempty"`  // 货币类型
+	Language string                 `json:"language,omitempty"`  // 日志来源系统 （PHP, GO, JAVA, NODEJS, Android, IOS, Linux
+	Version  string                 `json:"version,omitempty"`   // 代码版本号
+	Code     int                    `json:"code,omitempty"`      // 业务的状态码
+	Content  map[string]interface{} `json:"content,omitempty"`   // 自定义给日志打印方，随意key-value的方式即可
 }
