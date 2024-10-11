515328f4e6e616b13f74e0281c6918390a30196f
STones_ <61647649@qq.com>
2024-10-11 16:07:55 +0800

    add

--- pkg/k3/protocol/elk_data.go
+++ pkg/k3/protocol/elk_data.go
@@ -1,22 +1,27 @@
 package protocol
 
+// text => linux, vm,
+// json => nginx, alb, mysql, redis
+
+// _index =>
+
 type ElasticSearchData struct {
-	LogLevel   string     `json:"log_level,omitempty"`  // 日志级别
-	HostName   string     `json:"host_name,omitempty"`  // 日志落盘主机名
-	UUID       string     `json:"_id,omitempty"`        // 日志唯一ID， elk document id
+	UUID       string     `json:"_id,omitempty"`        // 日志唯一ID， elk document id  √
+	LogLevel   string     `json:"log_level,omitempty"`  // 日志级别 warn, error, debug, info
+	HostName   string     `json:"host_name,omitempty"`  // 日志落盘主机名 √
+	HostIp     string     `json:"host_ip,omitempty"`    // 日志落盘IP √
 	TraceId    string     `json:"trace_id,omitempty"`   // 追踪ID
 	Domain     string     `json:"domain,omitempty"`     // 域名
+	Protocol   string     `json:"protocol,omitempty"`   // 协议"HTTP/1.1"
 	HttpCode   int        `json:"http_code,omitempty"`  // http状态码
-	HostIp     string     `json:"host_ip,omitempty"`    // 日志落盘IP
-	CodeName   string     `json:"code_name,omitempty"`  // 代码仓库标识
 	ClientIp   string     `json:"client_ip,omitempty"`  // 日志来源IP
-	Timestamp  string     `json:"timestamp,omitempty"`  // 日志产生时间 "2024-10-01 12:00:00 "
-	Org        string     `json:"org,omitempty"`        // 事业部
-	LogSrc     string     `json:"log_src,omitempty"`    // 日志源(内部调用/调用第三方等)
-	Project    string     `json:"project,omitempty"`    // 业务线 业务中心， 公司系统, 投放等
+	Org        string     `json:"org,omitempty"`        // 事业部, gnfx, tapplus, zganme等
+	Project    string     `json:"project,omitempty"`    // 业务线 ywzx， company, tf, sdk4,等
+	CodeName   string     `json:"code_name,omitempty"`  // 代码仓库标识, user_service, nginx, mysql, redis等
+	LogSrc     string     `json:"log_src,omitempty"`    // 日志源(内部调用default/sms/phone等)
 	EventId    int        `json:"event_id,omitempty"`   // 日志事件ID
 	EventName  string     `json:"event_name,omitempty"` // 日志事件名称(每种日志唯一)
-	Protocol   string     `json:"protocol,omitempty"`   // 协议"HTTP/1.1"
+	Timestamp  string     `json:"timestamp,omitempty"`  // 日志产生时间 "2024-10-01 12:00:00 " √
 	ExtendData ExtendData `json:"extend_data"`          // 扩展字段
 }
 
