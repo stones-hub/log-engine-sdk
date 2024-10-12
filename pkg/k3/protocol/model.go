a7f5edac1c552dfedae312e84ecd492e93306a4a
STones_ <61647649@qq.com>
2024-10-12 08:42:57 +0800

    add

--- pkg/k3/protocol/model.go
+++ pkg/k3/protocol/model.go
@@ -4,12 +4,12 @@ import "time"
 
 // Data 需要提交给日志存储服务器的数据接口
 type Data struct {
+	UUID       string                 `json:"uuid,omitempty"`       // 日志唯一ID
 	AccountId  string                 `json:"account_id,omitempty"` // 账户ID
 	AppId      string                 `json:"app_id,omitempty"`     // APPID
+	Ip         string                 `json:"ip,omitempty"`         // 日志来源ID
 	Timestamp  time.Time              `json:"Timestamp"`            // 日志时间
 	EventName  string                 `json:"event_name,omitempty"` // 所读文件路径string
-	Ip         string                 `json:"ip,omitempty"`         // 日志来源ID
-	UUID       string                 `json:"uuid,omitempty"`       // 日志唯一ID
 	Properties map[string]interface{} `json:"properties"`           // 日志具体内容
 }
 
